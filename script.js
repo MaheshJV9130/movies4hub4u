3ca843f8b456f35b4034913f439e4e5c325c5bdd
MaheshJV9130 <maheshvispute082@gmail.com>
2025-03-06 11:15:35 +0530

    random movies req reduced and more eventlistners

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const userSearch = document.getElementById('search')
+const userSearch = document.getElementById('search');
 const searchBtn = document.getElementById('btn');
 let movieContainer = document.getElementsByClassName('movieContainer')[0];
 let movie;
 
-searchBtn.addEventListener('click', () => {
-    movie = userSearch.value
-    if(movie == "" || movie == undefined){
-        getData()
+// Common search function
+function handleSearch() {
+    movie = userSearch.value.trim();
+    if (movie === "" || movie === undefined) {
+        getData(); // Fetch default/random movies
+    } else {
+        movieContainer.innerHTML = "";
+        getMovies(movie); // Search movies
     }
-    if(getData){
-       movieContainer.innerHTML = "";
-       getMovies()
+}
+
+// For click button
+searchBtn.addEventListener('click', handleSearch);
+
+// For Enter key (on the input field!)
+userSearch.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        handleSearch();
     }
-})
+});
 async function getMovies() {
 
 try {
     let responce = await fetch(`https://imdb.iamidiotareyoutoo.com/search?q=${movie}`);
     let data = await responce.json();
     let moviesArr = data.description;
-    //  console.log(moviesArr)
     moviesArr.forEach(movieObj => {
-        // console.log(movieObj['#TITLE']);
 
         let movieCard = document.createElement('div');
         movieCard.classList.add('movie');
-        movieCard.innerHTML = `  <div class="img"><img src="${movieObj['#IMG_POSTER']}" alt=""></div>
+        movieCard.innerHTML = `  <div class="img"><img loading= "lazy" src="${movieObj['#IMG_POSTER']}" alt="${movieObj['#TITLE']}"></div>
         <div class="title">
             <p>${movieObj['#TITLE']} ${movieObj['#YEAR']}</p>
         </div>
@@ -39,21 +47,16 @@ try {
 }
 
 
-// For get random movies when document is load
 
 
-let url = 'https://jsonfakery.com/movies/random/100';
+let url = 'https://jsonfakery.com/movies/random/50';
 async function getData(){
     let responce = await fetch(url)
     let moviesArr = await responce.json();
-    // console.log(moviesArr)
-    moviesArr.forEach(movieObj => {
-        // console.log(movieObj.poster_path);
-        // console.log(movieObj.original_title);
-        // console.log(movieObj.release_date.split('/').pop());
+    moviesArr.forEach(movieObj => {;
         let movieCard = document.createElement('div');
         movieCard.classList.add('movie');
-        movieCard.innerHTML = `  <div class="img"><img src="${movieObj.poster_path}" alt=""></div>
+        movieCard.innerHTML = `  <div class="img"><img loading= "lazy" src="${movieObj.poster_path}" alt="${movieObj.original_title}"></div>
         <div class="title">
             <p>${movieObj.original_title} ${(movieObj.release_date.split('/').pop())}</p>
         </div>
